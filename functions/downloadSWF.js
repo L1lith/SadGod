70c3daed63b467208054e8e8b8d59efe9e6339eb
unknown <lily@l1lith.com>
2019-06-11 06:41:26 -0600

    update the package

--- functions/downloadSWF.js
+++ functions/downloadSWF.js
@@ -1,13 +1,18 @@
 const {join} = require('path')
 const SWFData = require('./SWFData')
 const download = require('download')
+const {exists} = require('fs-extra')
+
 
 const downloadFolder = join(__dirname, '..', 'downloads')
 
 async function downloadSWF(swfData) {
   if (!(swfData instanceof SWFData)) throw new Error("Expected SWF Data")
-  await download(swfData.url, downloadFolder)
-  return join(downloadFolder, swfData.name + '.swf')
+  const filePath = join(downloadFolder, swfData.name + '.swf')
+  if ((await exists(filePath)) !== true) {
+    await download(swfData.url, downloadFolder)
+  }
+  return filePath
 }
 
 module.exports = downloadSWF
